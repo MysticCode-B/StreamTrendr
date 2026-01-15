7f80168fdc2dcffe68645bed62dd2d8c76b04659
Brandon Cartagena <brancartagena@gmail.com>
2026-01-15 17:21:12 -0500

    Added code in ProviderBar

---
+++ frontend/src/components/ProviderBar.jsx
@@ -0,0 +1,13 @@
+
+export const ProviderBar = () => {
+    return (
+        <div className="w-full bg-[#355f75] py-3 flex justify-center items-center gap-8">
+            <span className="text-sm cursor-pointer">Netflix</span>
+            <span className="text-sm cursor-pointer">Hulu</span>
+            <span className="text-sm cursor-pointer">Amazon Prime</span>
+            <span className="text-sm cursor-pointer">Disney+</span>
+            <span className="text-sm cursor-pointer">HBO Max</span>
+            <span className="text-sm cursor-pointer">Apple TV+</span>
+        </div>
+    );
+}
