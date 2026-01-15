b2c68bff97dd83602fb69afd963464fe2c0f5c4e
Brandon Cartagena <brancartagena@gmail.com>
2026-01-15 16:53:27 -0500

    Imported Navbar & Dashboard in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Dashboard />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
+
