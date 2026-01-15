51d2bfc67ff63169be63ebfae75f29e250fadd6b
Brandon Cartagena <brancartagena@gmail.com>
2026-01-15 18:15:25 -0500

    Added const Hero in Hero.jsx

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+export const Hero = () => {
+    return (
+        <div className="flex justify-center mt-10">
+            <div className="w-[85%] h-[450px] rounded-xl border-4 border-[#6b8f9b] p-10 text-white">
+                <h2 className="text-4xl font-bold leading-tight">
+                    Check Out <br />
+                    The Latest <br />
+                    Trending <br />
+                    TV Shows <br />
+                    Or Movies
+                </h2>
+            </div>
+        </div>
+    );
+}
